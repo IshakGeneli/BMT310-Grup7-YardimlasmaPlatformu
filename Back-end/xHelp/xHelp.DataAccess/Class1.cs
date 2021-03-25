91a79aaa3410cb8dec1d668f2020abbb1f9720f9
ArifTarp <arifoklh@gmail.com>
2021-03-25 19:39:34 +0300

    Added data access operations

--- Back-end/xHelp/xHelp.DataAccess/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace xHelp.DataAccess
-{
-    public class Class1
-    {
-    }
-}
---
+++ Back-end/xHelp/xHelp.DataAccess/Concrete/EntityFrameworkCore/xHelpContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using xHelp.Entity.Concrete;
+
+namespace xHelp.DataAccess.Concrete.EntityFrameworkCore
+{
+    public class xHelpContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("");
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.ApplyConfiguration(new TaskMap());
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Task> Tasks { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Achievement> Achievements { get; set; }
+        public DbSet<Evidence> Evidences { get; set; }
+    }
+}
