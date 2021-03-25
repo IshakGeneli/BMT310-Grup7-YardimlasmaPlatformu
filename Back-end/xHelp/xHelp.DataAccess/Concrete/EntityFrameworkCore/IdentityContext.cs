a284e406d37467ac9a70ad02c748b0e938c42ad3
ArifTarp <arifoklh@gmail.com>
2021-03-25 23:22:30 +0300

    Added identity db context

---
+++ Back-end/xHelp/xHelp.DataAccess/Concrete/EntityFrameworkCore/IdentityContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using xHelp.Entity.Concrete;
+
+namespace xHelp.DataAccess.Concrete.EntityFrameworkCore
+{
+    public class IdentityContext : IdentityDbContext<User, UserRole, string>
+    {
+        public IdentityContext(DbContextOptions<IdentityContext> dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+    }
+}
