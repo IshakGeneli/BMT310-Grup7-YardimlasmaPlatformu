334212340ae3bcb921d2721f264fb99d18371705
ArifTarp <arifoklh@gmail.com>
2021-03-25 23:15:21 +0300

    Added entity relation mapping

---
+++ Back-end/xHelp/xHelp.DataAccess/Mappings/UserMap.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using xHelp.Entity.Concrete;
+
+namespace xHelp.DataAccess.Mappings
+{
+    public class UserMap : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.ToTable("Users");
+            builder.HasKey(u => u.Id);
+
+            builder.HasOne(u => u.Contact).WithOne(c => c.User).HasForeignKey<Contact>(c => c.UserId).OnDelete(DeleteBehavior.Cascade);
+            builder.HasMany(u => u.Achievements).WithOne(a => a.User).HasForeignKey(a => a.UserId).OnDelete(DeleteBehavior.Cascade);
+            builder.HasMany(u => u.Missions).WithOne(m => m.User).HasForeignKey(m => m.OwnerUserId).OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
