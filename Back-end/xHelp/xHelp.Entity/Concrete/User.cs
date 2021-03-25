d8b176ab0e267646754569d67b473644613deefd
ArifTarp <arifoklh@gmail.com>
2021-03-25 22:55:29 +0300

    Added identity entities

---
+++ Back-end/xHelp/xHelp.Entity/Concrete/User.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace xHelp.Entity.Concrete
+{
+    public class User : IdentityUser
+    {
+        public User()
+        {
+            Missions = new List<Mission>();
+            Achievements = new List<Achievement>();
+        }
+
+        public Contact Contact { get; set; }
+        public ICollection<Mission> Missions { get; set; }
+        public ICollection<Achievement> Achievements { get; set; }
+    }
+}
