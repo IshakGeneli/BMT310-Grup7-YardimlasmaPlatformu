2576768765ca4294f389ce515627800a88f8bd52
ArifTarp <arifoklh@gmail.com>
2021-03-28 20:03:10 +0300

    Added results

---
+++ Back-end/xHelp/xHelp.Core/Utilities/Results/Abstract/IDataResult.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace xHelp.Core.Utilities.Results.Abstract
+{
+    public interface IDataResult<T> : IResult
+    {
+        public T Data { get; set; }
+    }
+}
---
+++ Back-end/xHelp/xHelp.Core/Utilities/Results/Concrete/ErrorResult.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using xHelp.Core.Utilities.Results.Abstract;
+
+namespace xHelp.Core.Utilities.Results.Concrete
+{
+    public class ErrorResult : IResult
+    {
+        public int HttpStatusCode { get; set; }
+
+        public ErrorResult(HttpStatusCode httpStatusCode)
+        {
+            this.HttpStatusCode = Convert.ToInt32(httpStatusCode);
+        }
+    }
+}
