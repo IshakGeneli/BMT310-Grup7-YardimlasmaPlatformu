054aabc06a47ca285ff1d94773875ed96b557009
ArifTarp <arifoklh@gmail.com>
2021-03-28 19:49:57 +0300

    Added dtos

---
+++ Back-end/xHelp/xHelp.Entity/DTOs/UserRegisterDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace xHelp.Entity.DTOs
+{
+    public class UserRegisterDTO
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
