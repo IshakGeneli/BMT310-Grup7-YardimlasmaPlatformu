aad2f091758996539aaa2e149bf8630a0e2cbc16
ArifTarp <arifoklh@gmail.com>
2021-03-28 19:18:15 +0300

    Added new map

---
+++ Back-end/xHelp/xHelp.DataAccess/Mappings/EvidenceMap.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using xHelp.Entity.Concrete;
+
+namespace xHelp.DataAccess.Mappings
+{
+    public class EvidenceMap : IEntityTypeConfiguration<Evidence>
+    {
+        public void Configure(EntityTypeBuilder<Evidence> builder)
+        {
+            builder.ToTable("Evidences");
+            builder.HasKey(m => m.Id);
+        }
+    }
+}
