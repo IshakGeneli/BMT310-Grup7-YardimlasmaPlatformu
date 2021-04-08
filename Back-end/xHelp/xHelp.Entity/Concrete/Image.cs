1a4ecdcc12dd3cb5b9c162e90e55b88e126b8228
ArifTarp <arifoklh@gmail.com>
2021-04-08 21:05:25 +0300

    Changed image field

--- Back-end/xHelp/xHelp.Entity/Concrete/Image.cs
+++ Back-end/xHelp/xHelp.Entity/Concrete/Image.cs
@@ -14,7 +14,7 @@ namespace xHelp.Entity.Concrete
         }
 
         public int Id { get; set; }
-        public string FileName { get; set; }
+        public string Url { get; set; }
 
         public ICollection<EvidenceImage> EvidenceImages { get; set; }
         public ICollection<MissionImage> MissionImages { get; set; }
